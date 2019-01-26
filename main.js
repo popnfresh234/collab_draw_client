2dc7c3d4c1a76b3f07976a073566f95c7a50859b
Alex <ajhollid@gmail.com>
2019-01-25 16:47:50 -0800

    Use websocket to handle drawing

--- main.js
+++ main.js
@@ -3,22 +3,14 @@ const app = ( function () {
   const HEIGHT = window.innerHeight;
   const canvas = document.getElementById( 'collab_canvas' );
   const ctx = canvas.getContext( '2d' );
+  let socket = null;
   const mouseStatus = {
     down: false,
   };
   let points = [];
 
-  const setup = () => {
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
-    canvas.style.border = '1px solid black';
-  };
-
-  const getViewportWidth = () => WIDTH;
-  const getViewportHeight = () => HEIGHT;
   const drawLine = ( pointsArray, context ) => {
     if ( pointsArray.length > 1 ) {
-      console.log( pointsArray );
       context.beginPath();
       context.moveTo(
         pointsArray[pointsArray.length - 2].normalizedX * WIDTH,
@@ -31,13 +23,31 @@ const app = ( function () {
       context.stroke();
     }
   };
+
+  const setup = () => {
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    canvas.style.border = '1px solid black';
+    socket = new WebSocket( 'ws://localhost:8080/' );
+    socket.onmessage = ( msg ) => {
+      drawLine( JSON.parse( msg.data ), ctx );
+    };
+  };
+
+  const sendData = ( data ) => {
+    socket.send( data );
+  };
+
+  const getViewportWidth = () => WIDTH;
+  const getViewportHeight = () => HEIGHT;
+
   const addListeners = () => {
     canvas.addEventListener( 'mousemove', ( e ) => {
       if ( mouseStatus.down ) {
         const normalizedX = e.clientX / WIDTH;
         const normalizedY = e.clientY / HEIGHT;
         points.push( { normalizedX, normalizedY } );
-        drawLine( points, ctx );
+        sendData( JSON.stringify( points ) );
       }
     } );
 
